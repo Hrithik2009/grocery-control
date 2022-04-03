cb4a36727cb8d12bb145e4eaa0879e72479ab5ed
hrithikchr <hrithik.pandey@chrobinson.com>
2022-04-04 03:44:05 +0530

    edit and delete completed

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,4 @@
+const req = require('express/lib/request');
 const Product = require('../models/Product');
 
 const getProducts = async(req, res) => {
@@ -11,7 +12,7 @@ const getProducts = async(req, res) => {
 
 const addProduct = async (req, res) => {
     const shop_email = res.locals.user.email;
-    const product_name = req.body.product_name;
+    const product_name = req.body.product_name.toLowerCase();
     const expiry_date = req.body.expiry_date; 
     const price = parseInt(req.body.price);
     const quantity = parseInt(req.body.quantity);
@@ -25,4 +26,42 @@ const addProduct = async (req, res) => {
     }
 };
 
-module.exports = {addProduct, getProducts};
+const getEditproduct = async(req, res) =>{
+    const {id: productId} = req.params;
+    let product = await Product.findOne({_id: productId});
+    res.render('edit_product', {
+        product: product
+    });
+}
+
+const editproduct = async(req, res) =>{
+    const shop_email = res.locals.user.email;
+    const product_name = req.body.product_name;
+    const expiry_date = req.body.expiry_date; 
+    const price = parseInt(req.body.price);
+    const quantity = parseInt(req.body.quantity);
+
+    try{
+        const product = await Product.findOneAndUpdate({shop_email: shop_email, product_name: product_name}, {expiry_date, price, quantity}, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({product});
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+const deleteProduct = async(req, res) => {
+    const productId = req.body.productID;
+    try{
+        const delProduct = await Product.findOneAndDelete({_id: productId});
+        res.status(200).json({status: 'Success'});
+    }
+    catch(err){
+        res.status(500).json({msg: err});
+    }
+}
+
+module.exports = {addProduct, getProducts, getEditproduct, editproduct, deleteProduct};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,6 +4,9 @@ const productController = require('../controllers/productController');
 const {auths, checkUser} = require('../middleware/auth');
 
 router.get('/products', auths, checkUser, productController.getProducts);
-router.post('/addproducts', checkUser, productController.addProduct);
+router.post('/addproducts', auths, checkUser, productController.addProduct);
+router.get('/editproduct/:id', auths, checkUser, productController.getEditproduct);
+router.patch('/editproduct', auths, checkUser, productController.editproduct);
+router.delete('/delproduct', auths, checkUser, productController.deleteProduct);
 
 module.exports = router;
